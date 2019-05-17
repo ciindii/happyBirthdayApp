038c71913b325bc59aa74ddca460521752e3c231
Cindy Truong <cindythitruong@gmail.com>
2019-05-16 23:59:32 -0700

    updated input and created message functions

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
+//constructor
 
+// class Person{
+//   constructor(birthday, yearBorn) {
+//     this.birthday = birthday;
+//     this.yearBorn = yearBorn;
+//   }
+// }
+//
+// Person.prototype.getSummary = function() {
+//   return `${this.birthday} is your birthday`;
+// };
+//
+// let cindy = new Person('05-13','1994');
+//
+// const getBirthday = () => {
+//   let userBirthday = prompt('When is your birthday?');
+//   return userBirthday;
+// };
+// getBirthday();
+//
+// console.log(cindy.getSummary());
+
+
+// function sayHappyBirthday(){
+//   let q = new Date();
+//   let month = q.getMonth();
+//   let day = q.getDay();
+//   let year = q.getFullYear();
+//   let
+//
+//   let date = new Date(month, day);
+//   if( date == Person){}
+// }
+let month  = [];
+let day   = [];
+let year = [];
+
+let monthInput  = document.getElementById("month");
+let dayInput   = document.getElementById("day");
+let yearInput = document.getElementById("year");
+
+let messageBox  = document.getElementById("display");
+
+function insert ( ) {
+ month.push( monthInput.value );
+ day.push( dayInput.value );
+ year.push( yearInput.value );
+
+ clearAndShow();
+ calcYearsOld();
+}
+
+function clearAndShow () {
+  // Clear our fields
+  monthInput.value = "";
+  dayInput.value = "";
+  yearInput.value = "";
+
+  // Show our output
+  messageBox.innerHTML = "";
+  //
+  let q = new Date();
+  let m = q.getMonth();
+  let d = q.getDay();
+  let date = new Date(m, d);
+  if( m == month && d == day ){
+    messageBox = `Happy Birthday!`;
+  }else{
+    messageBox.innerHTML += `Your birthday is ${month}/${day}`;
+  }
+}
