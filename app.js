5060b1fb065d6d9b15f559ffcb3e04f9ce6ea8da
Cindy Truong <cindythitruong@gmail.com>
2019-05-18 23:37:06 -0700

    compare input to date function

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ let yearInput = document.getElementById("year");
 
 let messageBox  = document.getElementById("display");
 
+//puts input into array and run clearAndShow()
 function insert ( ) {
  month.push( monthInput.value );
  day.push( dayInput.value );
- year.push( yearInput.value );
 
  clearAndShow();
 }
@@ -55,23 +55,52 @@ function clearAndShow () {
   // Clear our fields
   monthInput.value = "";
   dayInput.value = "";
-  yearInput.value = "";
+  // yearInput.value = "";
 
   // Show our output
   messageBox.innerHTML = "";
 
-  // let q = new Date();
-  // let m = q.getMonth();
-  // let d = q.getDay();
-  // let date = new Date(m, d);
-
   var d = new Date();
   if (
-    Number(document.getElementById("month").value) === (d.getMonth() + 1) &&
-    Number(document.getElementById("day").value) === d.getDate()
+    Number(document.getElementById("month").value) == (d.getMonth() + 1) &&
+    Number(document.getElementById("day").value) == d.getDate()
   ) {
     messageBox.innerHTML = `Happy Birthday!`;
   } else {
-    messageBox.innerHTML += `Your birthday is ${month}/${day}`;
+    messageBox.innerHTML = `Your birthday is ${month}/${day}`;
   }
 }
+
+var a = new Date();
+console.log(a);
+console.log(a.getMonth()+1);
+console.log(a.getDate());
+
+function run(month1, day1){
+if (
+  month1 === (a.getMonth() + 1) && day1 === a.getDate()
+) {
+  console.log('it works');
+} else {
+  console.log('it failed');
+  }
+}
+
+
+run(5,18);
+run(3,18);
+
+function parse (x, y) {
+  let monthNum = parseInt(x);
+  let dayNum = parseInt(y);
+  var d = new Date();
+  if (
+    monthNum == (d.getMonth() + 1) &&
+    dayNum == d.getDate()
+  ) {
+    console.log('HBD');
+  } else {
+    console.log('lame');
+}
+}
+parse(5,18);
